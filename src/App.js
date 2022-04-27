49d6868c2bf9fa050c3c5d9e3149bf2d3da5520a
Devin-Symone Johnson <devinsymone@gmail.com>
2022-04-27 17:18:55 -0400

    deleted unnessary files and motified app.js to start title page:

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+
+    const [music, setMusic] = useState([{title:'', artist: '', genre:'', release_date:'', likes:'' }])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h1> Music Library</h1>
+  <h3>Welcome to your Music Library</h3>
+  <table>
+    <thread>
+      <tr>
+        <th> Title </th>
+        <th> Artist </th>
+        <th> Album </th>
+        <th> Genre </th>
+        <th> Likes </th>
+      </tr>
+    </thread>
+  </table>
+  </div>
   );
 }
 
